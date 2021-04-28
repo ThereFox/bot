4c6643fcffac3d91120a0e99a6284d0e48acd9b9
ThereFox <59343016+ThereFox@users.noreply.github.com>
2021-04-28 15:28:39 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    
 <?php
 if (empty($data['message']['chat']['id'])) {
 	exit();
@@ -33,3 +41,6 @@ if (!empty($data['message']['text'])) {
 
 		exit();
 	}
+?>
+  </body>
+</html>
